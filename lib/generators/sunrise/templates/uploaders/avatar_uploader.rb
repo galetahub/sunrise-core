0bba82b560ef232af092386e911e3f5d61c104d6
superp <superp1987@gmail.com>
2011-08-25 14:39:20 +0300

    add version :small to AvatarUploader

--- lib/generators/sunrise/templates/uploaders/avatar_uploader.rb
+++ lib/generators/sunrise/templates/uploaders/avatar_uploader.rb
@@ -7,6 +7,10 @@ class AvatarUploader < Sunrise::CarrierWave::BaseUploader
     process :resize_to_fill => [100, 100]
   end
   
+  version :small do
+    process :resize_to_fill => [32, 32]
+  end
+  
   def extension_white_list
     %w(jpg jpeg gif png)
   end
